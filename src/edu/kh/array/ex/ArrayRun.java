e99a0b11c830179a565641510fc9364e591c56bd
SunMinK <sunnysunm98@gmail.com>
2022-12-14 14:27:46 +0900

    Array

---
+++ src/edu/kh/array/ex/ArrayRun.java
@@ -0,0 +1,13 @@
+package edu.kh.array.ex;
+
+public class ArrayRun {
+
+	public static void main(String[] args) {
+		 //ArrayExample1 ex = new ArrayExample1();
+		 //ex.ex9();
+		 
+		 ArrayExample2 ex2 = new ArrayExample2();
+		 ex2.shallowCopy();
+	}
+
+}
